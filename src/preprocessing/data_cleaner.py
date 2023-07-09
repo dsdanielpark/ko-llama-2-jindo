914ce311ddf838f87c602fcd5698882cabe37d28
parkminwoo <parkminwoo1991@gmail.com>
2023-07-09 13:36:33 +0900

    doc: updated

--- src/preprocessing/data_cleaner.py
+++ src/preprocessing/data_cleaner.py
@@ -10,231 +10,168 @@ Description: This file should be executed using Python 3.8 or above.
 import json
 import concurrent.futures
 import re
-import src.config as CONF
-from typing import List
 
 
-class DataCleaner:
-    """
-    DataCleaner class for processing and cleaning JSON data.
-    """
+class DataProcessor:
+    def __init__(self, target_file_path):
+        with open(target_file_path) as f:
+            self.data = json.load(f)
+        self.excluded_data = []
 
-    def __init__(self, target_file: str, steps: List[str], preprocessing_param: dict):
-        """
-        Initialize the DataCleaner object.
+    @staticmethod
+    def remove_specific_words(data):
+        word_set = {'sure!', 'great!', 'Certainly!', "Sure!", "Great!", "Great, ", "great, ",
+                    "sure, ", "Sure, ", "Sure! "}
+        return [d for d in data if not any(word in d['input'] for word in word_set)]
 
-        Args:
-            target_file (str): Path to the target JSON file.
-            steps (List[str]): List of steps to perform during data cleaning.
-            preprocessing_param (dict): Preprocessing parameters containing the prefix set,
-                                        dummy file path, phrase dictionary, and remove target word set.
-        """
-        self.target_file = target_file
-        self.steps = steps
-        self.prefix_set = preprocessing_param['prefix_set']
-        self.dummy_file_path = preprocessing_param['dummy_file_path']
-        self.phrase_dictionary = preprocessing_param['phrase_dictionary']
-        self.remove_target_word_set = preprocessing_param['remove_target_word_set']
+    @staticmethod
+    def remove_short_fields(data):
+        try:
+            return [d for d in data if (input_len := len(d['input'])) > 4 and (output_len := len(d['output'])) > 4]
+        except Exception as e:
+            print(f"Error at remove_short_fields {e} \n Finished unsuccessfully.")
+            return data
 
-    def remove_specific_words(self, data: List[dict]) -> List[dict]:
-        """
-        Remove specific words from the input data.
+    @staticmethod
+    def replace_sure_translation(data):
+        for d in data:
+            try:
+                d['output'] = re.sub(r'\b물론,\b', '물론이죠.', d['output'])
+            except Exception as e:
+                print(f"Error at replace_sure_translation {e}")
+        return data
 
-        Args:
-            data (List[dict]): Input data.
+    @staticmethod
+    def delete_error_korean_prefix(data):
+        for d in data:
+            try:
+                d['output'] = re.sub(r'^(은 |는 )', '', d['output'])
+            except Exception as e:
+                print(f"Error at delete_error_korean_prefix {e}")
+        return data
 
-        Returns:
-            List[dict]: Processed data with specific words removed.
-        """
-        return [d for d in data if not any(word in d['input'] for word in self.remove_target_word_set)]
-
-    def remove_short_fields(self, data: List[dict]) -> List[dict]:
-        """
-        Remove data fields with input and output lengths less than or equal to 4.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with short fields removed.
-        """
-        return [d for d in data if (input_len := len(d['input'])) > 4 and (output_len := len(d['output'])) > 4]
-
-    def replace_phrase(self, data: List[dict]) -> List[dict]:
-        """
-        Replace phrases in the output data using the provided phrase dictionary.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with phrases replaced.
-        """
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            return list(executor.map(self.replace_sure_translation, data))
-
-    def replace_sure_translation(self, d: dict) -> dict:
-        """
-        Replace "물론," with "물론이죠." and remove Korean prefixes from the output data.
-
-        Args:
-            d (dict): Input dictionary.
-
-        Returns:
-            dict: Processed dictionary with phrases replaced and prefixes removed.
-        """
-        for pattern, replacement in self.phrase_dictionary.items():
-            d['output'] = re.sub(pattern, replacement, d['output'])
-        return d
-
-    def delete_error_korean_prefix(self, d: dict) -> dict:
-        """
-        Delete error Korean prefixes (은,는) from the output data.
-
-        Args:
-            d (dict): Input dictionary.
-
-        Returns:
-            dict: Processed dictionary with error Korean prefixes removed.
-        """
-        d['output'] = re.sub(r'^(은 |는 )', '', d['output'])
-        return d
-
-    def replace_prefix(self, data: List[dict]) -> List[dict]:
-        """
-        Replace prefixes in the output data.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with prefixes replaced.
-        """
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            return list(executor.map(self.delete_error_korean_prefix, data))
-
-    def do_not_translate_code_snippet(self, d: dict) -> dict:
-        """
-        Do not translate code snippets in the input and output data.
-
-        Args:
-            d (dict): Input dictionary.
-
-        Returns:
-            dict: Processed dictionary with code snippets unchanged.
-        """
-        input_text = d['input']
-        output_text = d['output']
-
-        if '```' in input_text and '```' in output_text:
-            start_index = input_text.find('```') + 3
-            end_index = input_text.find('```', start_index)
-            replace_text = input_text[start_index:end_index]
-
-            d['output'] = output_text.replace('```', f' ```{replace_text}```')
-
-        return d
-
-    def replace_common_phrase(self, data: List[dict]) -> List[dict]:
-        """
-        Replace common phrases in the output data.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with common phrases replaced.
-        """
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            return list(executor.map(self.do_not_translate_code_snippet, data))
-
-    def remove_duplicate_input(self, data: List[dict]) -> List[dict]:
-        """
-        Remove duplicate input data.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with duplicate input removed.
-        """
-        unique_inputs = set()
+    @staticmethod
+    def replace_output_prefix(data):
+        prefix_set = {"을 ", "를 ", "이 ", "가 ", "h", "은 ", "는 ", "에 ", "으 ", "의", "예, ", "^[A-Za-z] ", "^[ㄱ-ㅎㅏ-ㅣ가-힣] ", "^[0-9] ", ".", ","}
         result = []
 
         for d in data:
-            input_text = d['input']
-            if input_text not in unique_inputs:
-                unique_inputs.add(input_text)
-                result.append(d)
-
+            try:
+                output_text = d['output']
+                if not output_text.startswith(tuple(prefix_set)):
+                    result.append(d)
+            except Exception as e:
+                print(f"Error at replace_output_prefix {e}")
+            
         return result
 
-    def replace_output_prefix(self, data: List[dict]) -> List[str]:
-        """
-        Replace output prefixes.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[str]: List of output prefixes.
-        """
-        dummy_data = [d['output'] for d in data if d['output'].startswith(tuple(self.prefix_set))]
-        return dummy_data
-
-    def save_and_remove_prefix(self, data: List[dict]) -> List[dict]:
-        """
-        Save and remove output prefixes.
-
-        Args:
-            data (List[dict]): Input data.
-
-        Returns:
-            List[dict]: Processed data with prefixes saved and removed.
-        """
-        dummy_data = [d['output'] for d in data if d['output'].startswith(tuple(self.prefix_set))]
-
-        with open(self.dummy_file_path, 'w', encoding='utf-8') as f:
-            json.dump(dummy_data, f, ensure_ascii=False, indent=4, separators=(',', ':'))
-
-        return [d for d in data if not any(d['output'].startswith(prefix) for prefix in self.prefix_set)]
-
-    def process_json_file(self):
-        """
-        Process the JSON file based on the specified steps and save the cleaned data.
-        """
-        with open(self.target_file) as f:
-            data = json.load(f)
-
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            if 'step1' in self.steps:
-                data = list(executor.map(self.remove_specific_words, [data]))
-
-            if 'step2' in self.steps:
-                data = list(executor.map(self.remove_short_fields, [data]))
-
-            if 'step3' in self.steps:
-                data = list(executor.map(self.replace_phrase, [data]))
-
-            if 'step4' in self.steps:
-                data = list(executor.map(self.replace_prefix, [data]))
-
-            if 'step5' in self.steps:
-                data = list(executor.map(self.replace_common_phrase, [data]))
-
-            if 'step6' in self.steps:
-                data = list(executor.map(self.remove_duplicate_input, [data]))
-
-            if 'step7' in self.steps:
-                data = list(executor.map(self.replace_output_prefix, [data]))
-
-            if 'step8' in self.steps:
-                data = list(executor.map(self.save_and_remove_prefix, [data]))
-
-        with open(self.target_file, "w", encoding="utf-8") as file:
-            json.dump(data[0], file, ensure_ascii=False, indent=4, separators=(',', ':'))
 
 
-if __name__ == "__main__":
-    cleaner = DataCleaner(CONF.preprocessing_param['target_json_file_path'], ['step1', 'step2', 'step3'], preprocessing_param=CONF.preprocessing_param)
-    cleaner.process_json_file()
+    @staticmethod
+    def do_not_translate_code_snippet(data):
+        for d in data:
+            try:
+                input_text = d['input']
+                output_text = d['output']
+
+                if '```' in input_text and '```' in output_text:
+                    start_index = input_text.find('```') + 3
+                    end_index = input_text.find('```', start_index)
+                    replace_text = input_text[start_index:end_index]
+
+                    d['output'] = output_text.replace('```', f' ```{replace_text}```')
+            except Exception as e:
+                print(f"Error at do_not_translate_code_snippet {e}")
+
+        return data
+
+    @staticmethod
+    def remove_duplicates(data):
+        unique_data = []
+        seen_inputs = set()
+        seen_outputs = set()
+        for d in data:
+            input_value = d["input"]
+            output_value = d["output"]
+            if (input_value, output_value) not in seen_inputs and \
+                    (input_value, output_value) not in seen_outputs and \
+                    input_value != output_value:
+                seen_inputs.add((input_value, output_value))
+                seen_outputs.add((input_value, output_value))
+                unique_data.append(d)
+        return unique_data
+
+    @staticmethod
+    def remove_deletion_and_addition(data):
+        for d in data:
+            input_value = d["input"]
+            output_value = d["output"]
+
+            input_words = input_value.split()
+            output_words = output_value.split()
+
+            if len(output_words[0]) > 1 and len(input_words[0]) > 2:
+                if len(set(output_words[0].lower()) - set(input_words[0].lower())) < 2 and \
+                        input_words[0][1].lower() == output_words[0][0].lower() and \
+                        input_words[0][2].lower() == output_words[0][1].lower():
+                    output_words[0] = input_words[0]
+                    output_value = " ".join(output_words)
+                    d["output"] = output_value
+
+        return data
+
+    @staticmethod
+    def flatten_list(data):
+        flattened_list = []
+        for sublist in data:
+            if isinstance(sublist, list):
+                flattened_list.extend(DataProcessor.flatten_list(sublist))
+            else:
+                flattened_list.append(sublist)
+        return flattened_list
+
+    @staticmethod
+    def write_to_file(data, file_path):
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4, separators=(',', ':'))
+
+
+    def process_json_file(self, steps, output_file_path, dummy_file_path):
+        for step in steps:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                if 'step1' in step:
+                    self.data = list(executor.map(DataProcessor.remove_specific_words, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step2' in step:
+                    self.data = list(executor.map(DataProcessor.remove_short_fields, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step3' in step:
+                    self.data = list(executor.map(DataProcessor.replace_sure_translation, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step4' in step:
+                    self.data = list(executor.map(DataProcessor.delete_error_korean_prefix, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step5' in step:
+                    self.data = list(executor.map(DataProcessor.do_not_translate_code_snippet, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step6' in step:
+                    self.data = list(executor.map(DataProcessor.remove_duplicates, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+                if 'step7' in step:
+                    self.excluded_data = list(executor.map(self.replace_output_prefix, [self.data]))
+                    self.excluded_data = DataProcessor.flatten_list(self.excluded_data)
+                    self.data = [d for d in self.data if d not in self.excluded_data]
+
+                if 'step8' in step:
+                    self.data = list(executor.map(DataProcessor.remove_deletion_and_addition, [self.data]))
+                    self.data = DataProcessor.flatten_list(self.data)
+
+        self.data = DataProcessor.flatten_list(self.data)
+        self.write_to_file(self.data, output_file_path)
+        self.write_to_file(self.excluded_data, dummy_file_path)
